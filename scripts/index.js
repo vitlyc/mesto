d636b11644dddc3851d7f0987f48d6144d24643a
vitlyc <72764133+vitlyc@users.noreply.github.com>
2021-06-26 19:43:02 +0300

    5 sprint delayu

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,58 @@
-// console.log()
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+const elementsList = document.querySelector('.elements__list')
+const template = document.querySelector('.template').content
+
+
+
+initialCards.forEach(function(item) {
+
+    makingElement(item)
+})
+
+
+
+
+function makingElement(item) {
+    const newElement = template.cloneNode(true)
+    newElement.querySelector('.element__title').textContent = item.name
+    newElement.querySelector('.element__image').src = item.link
+    elementsList.appendChild(newElement)
+}
+
+const elementRemove = document.querySelector('.element__remove-button')
+
+const addButton = document.querySelector('.profile__add-button')
+const addPopupForm = document.querySelector('.add-popup__container')
+const addPopupElement = document.querySelector('.add-popup')
+const addPopupClose = addPopupElement.querySelector('.add-popup__close-button')
+const addPopupSave = addPopupElement.querySelector('.add-popup__save-button')
+const addPopupFieldPlace = document.querySelector('.add-popup__text_type_place')
+const addPopupFieldSource = document.querySelector('.add-popup__text_type_source')
+
 const popupForm = document.querySelector('.popup__container')
 const popupElement = document.querySelector('.popup')
 const popupClose = popupElement.querySelector('.popup__close-button')
@@ -12,27 +66,64 @@ const profileSubtitle = document.querySelector('.profile__subtitle')
 const elementHeart = document.querySelectorAll('.element__heart')
 const elements = document.querySelector('.elements')
 const elementTitle = document.querySelector('.element__title')
-const togglePopupVisibility = function(evt) {
-    evt.preventDefault()
+
+function togglePopupVisibility(event) {
+    event.preventDefault()
     popupElement.classList.toggle('popup_opened')
 }
 
-const saveForm = function(evt) {
-    evt.preventDefault()
-    profileTitle.textContent = popupFieldName.value
-    profileSubtitle.textContent = popupFieldProf.value
-    popupElement.classList.toggle('popup_opened')
+function toggleAddpopupVisibility(event) {
+    event.preventDefault()
+    addPopupElement.classList.toggle('add-popup_opened')
 }
+
+
+const saveForm = function(evt) {
+    // console.log(evt.target.classList[0]);
+    if (evt.target.classList[0] == 'popup__container') {
+        evt.preventDefault();
+        profileTitle.textContent = popupFieldName.value;
+        profileSubtitle.textContent = popupFieldProf.value;
+        popupElement.classList.toggle('popup_opened');
+    } else if (evt.target.classList[0] == 'add-popup__container') {
+        evt.preventDefault();
+        addPopupElement.classList.toggle('add-popup_opened')
+        let newItem = {
+            name: "",
+            link: ""
+        };
+        newItem.name = addPopupFieldPlace.value
+        newItem.link = addPopupFieldSource.value
+        console.log(newItem);
+        makingElement(newItem)
+    }
+
+
+}
+
+
+
+
+
 
 profileEdit.addEventListener('click', togglePopupVisibility)
 popupClose.addEventListener('click', togglePopupVisibility)
-popupForm.addEventListener('submit', saveForm)
+addButton.addEventListener('click', toggleAddpopupVisibility)
+addPopupClose.addEventListener('click', toggleAddpopupVisibility)
 
+
+popupForm.addEventListener('submit', saveForm)
+addPopupForm.addEventListener('submit', saveForm)
 elements.addEventListener('click', function(event) {
     let target = event.target;
-    // console.log(target.classList[0])
-    if (target.classList[0] != 'element__heart') return;
-    target.classList.toggle('element__heart_active')
+
+    if (target.classList[0] == 'element__heart') {
+        target.classList.toggle('element__heart_active')
+    } else if (target.classList[0] == 'element__remove-button') {
+        event.target.closest('.element').remove()
+
+    }
+
 
 })
 
